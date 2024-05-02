5185f7bafa85cbd7b8b9e3d25b82b30c15e7ec2b
Dev <flashytesting@gmail.com>
2024-05-02 17:25:27 +0530

    fix: center layout of main page on max width

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,7 @@
 import { registerUser } from "../controller/auth.controller";
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
 
-const routes = async (app: FastifyInstance, options: FastifyPluginOptions) => {
-
+const routes = async (app: FastifyInstance, _options: FastifyPluginOptions) => {
   //register new user 
   app.post("/register", registerUser)
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,16 +4,17 @@ import routes from "./routes/routes";
 
 const PORT: number = Number(process.env.SERVER_PORT) || 3333;
 const app: FastifyInstance = fastify({
-  logger: true
+    logger: true
 })
 
 // routes
 app.register(routes, { prefix: "/" });
 
 app.listen({ port: PORT }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+
+    console.log(`Server listening at ${address}`)
 })
