395b8af9344028d0e0a29af72349fa4000abcae3
Dev <flashytesting@gmail.com>
2024-05-04 21:39:06 +0530

    feat: add API endpoint for deleting a post

--- server/src/controllers/posts.controller.ts
+++ server/src/controllers/posts.controller.ts
@@ -1,9 +1,10 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { pool } from "../database/db";
 import { createPostsTable } from "../database/posts";
-import type { QueryResult } from "pg";
+import type { Query, QueryResult } from "pg";
 import type {
     CreatePostReq,
+    DeletePostReq,
     GetPostReq,
     PostType
 } from "../types/posts";
@@ -97,8 +98,63 @@ const createPost = async (req: FastifyRequest<CreatePostReq>, res: FastifyReply)
     }
 }
 
+const deletePost = async (req: FastifyRequest<DeletePostReq>, res: FastifyReply) => {
+    const { postId } = req.params;
+    const username = req.username;
+
+    try {
+        // CHeck for post existance
+        await createPostsTable();
+        // NOTE: Check if this is needed or not
+        const postResult: QueryResult<{ id: string }> = await pool.query(`
+            SELECT id
+            FROM posts
+            WHERE posts.id = $1
+        `, [postId])
+
+        const postExists = postResult.rows[0];
+
+        if (!postExists) {
+            const error = 'No post found';
+            return res.status(404).send({ error });
+        }
+
+        // Check for user being authenticated for operation
+        const userResult: QueryResult<{ username: string }> = await pool.query(`
+            SELECT username
+            FROM posts
+            JOIN users
+            ON posts.user_id = users.id
+            WHERE posts.id = $1
+        `, [postId])
+
+        const user = userResult.rows[0];
+
+        if (user.username != username) {
+            const error = 'Anauthorized action';
+            return res.status(401).send({ error });
+        }
+
+        // Actually deleting 
+        // TODO: Find a way to get username from RETURNING
+        const result: QueryResult<Omit<PostType, 'username'>> = await pool.query(`
+            DELETE FROM posts
+            WHERE id = $1
+            RETURNING title, body, likes, dislikes
+        `, [postId])
+
+        const post = result.rows[0];
+
+        res.send(post);
+    } catch (error) {
+        console.error("Error occured on deleting a post.", error);
+        throw error;
+    }
+}
+
 export {
     getAllPosts,
     getPost,
-    createPost
+    createPost,
+    deletePost
 };
