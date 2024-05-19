948513835789dffa5a894f9c25a0acd8af8b458d
falcon71181 <abhaythakur71181@gmail.com>
2024-05-19 11:30:30 +0530

    feat: dummy forum post card

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,37 @@
+import { FormPostCard } from "@/components/community-form/FormPostCard";
+import { PlusIcon } from "@radix-ui/react-icons";
+
+type FormPostCardProps = {
+  title: string,
+  description: string
+}
+
+const dumy: FormPostCardProps = {
+  title: "dummy title",
+  description: "fdummmy description kffkkfkfk fk",
+}
+
 export default function Home() {
   return (
-    <main className="font-caveat text-2xl border-2 border-cyan-300">
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+    <main className="text-2xl p-5 flex justify-center items-center">
+      <div className="w-10/12 flex flex-col gap-5 items-center">
+        <div className="w-2/3 flex justify-between font-caveat text-center text-4xl px-5 py-8 tracking-wide">
+          <h1>Community Forum</h1>
+          <CreatePost />
+        </div>
+        <div className="w-full flex flex-col items-center justify-center">
+          <FormPostCard PostData={dumy} />
+        </div>
+      </div>
     </main>
   );
+}
+
+const CreatePost = () => {
+  return (
+    <div className="font-semibold cursor-pointer font-sans tracking-normal text-lg flex justify-center items-center rounded-md px-3 gap-2 bg-background shadow-sm hover:bg-accent hover:text-accent-foreground border border-accent">
+      <PlusIcon className="text-xl" />
+      <h1 className="text-sm">Create Post</h1>
+    </div>
+  )
 }
--- client/src/components/community-form/FormPostCard.tsx
+++ client/src/components/community-form/FormPostCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
-import { ThumbsUp, ThumbsDown, UserRound } from "lucide-react";
+import { ChevronUpIcon, ChevronDownIcon, PersonIcon } from "@radix-ui/react-icons";
 
 type FormPostCardProps = {
   title: string,
@@ -24,14 +24,14 @@ export function FormPostCard({ PostData }: { PostData: FormPostCardProps }) {
     >
       {/* TODO: Style better for light theme */}
       <motion.div
-        className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
+        className="pointer-events-none dark:absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
           background: useMotionTemplate`
 						radial-gradient(200px circle at ${mouseX}px ${mouseY}px, rgba(38, 38, 38, 0.6), transparent 80%)
 					`,
         }}
       />
-      <div className="h-full relative flex rounded-md border border-white/10">
+      <div className="h-full relative flex rounded-md border border-white/10 rounded-l-2xl">
         <LikeDislikeSection />
         <PostSection PostData={PostData} />
       </div>
@@ -41,13 +41,13 @@ export function FormPostCard({ PostData }: { PostData: FormPostCardProps }) {
 
 const LikeDislikeSection = () => {
   return (
-    <section className="cursor-pointer dark:bg-neutral-900 px-4 flex flex-col rounded-l-xl">
-      <div className="group/like h-1/2 flex flex-col gap-2 items-center justify-center">
-        <ThumbsUp className="dark:text-neutral-300 group-hover/like:text-green-300" />
+    <section className="cursor-pointer dark:bg-neutral-900 flex flex-col rounded-l-2xl">
+      <div className="px-4 group/like h-1/2 flex flex-col gap-2 items-center justify-center rounded-tl-2xl hover:bg-green-300/30">
+        <ChevronUpIcon className="dark:text-neutral-300 group-hover/like:text-green-300" />
         <span className="text-xs font-semibold dark:text-neutral-100 group-hover/like:text-neutral-50">1001</span>
       </div>
-      <div className="group/dislike h-1/2 flex flex-col gap-2 items-center justify-center">
-        <ThumbsDown className="dark:text-neutral-300 group-hover/dislike:text-red-300" />
+      <div className="px-4 group/dislike h-1/2 flex flex-col gap-2 items-center justify-center rounded-bl-2xl hover:bg-red-300/30">
+        <ChevronDownIcon className="dark:text-neutral-300 group-hover/dislike:text-red-300" />
         <span className="text-xs font-semibold dark:text-neutral-100 group-hover/dislike:text-neutral-50">1001</span>
       </div>
     </section>
@@ -64,7 +64,7 @@ const PostSection = ({ PostData }: { PostData: FormPostCardProps }) => {
       <div className="h-[20%] text-lg dark:text-neutral-100 font-semibold line-clamp-1">{PostData.title}</div>
       <div className="h-[25%] text-sm dark:text-neutral-300 line-clamp-2">{PostData.description}</div>
       <div className="h-[30%] flex gap-2 items-center">
-        <UserRound className="dark:text-neutral-100" />
+        <PersonIcon className="dark:text-neutral-100" />
         <span className="text-sm">username</span>
       </div>
     </section>
