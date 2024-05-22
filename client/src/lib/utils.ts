5687ccfdc07cb1984b15aa97e042b1f49aa817f2
Dev <flashytesting@gmail.com>
2024-05-23 00:19:50 +0530

    feat: add date formater util

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
+import { format } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getDate(date: Date) {
+    const formatedDate = format(date, 'do MMM yyyy');
+    return formatedDate;
 }
